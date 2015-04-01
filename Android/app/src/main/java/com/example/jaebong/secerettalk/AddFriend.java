112860a2992df72ce265fed1f6794ff27e321ce4
JaeBong <ljb0484@nhnnext.org>
2015-04-01 17:42:28 +0900

    Calendar & searchIcon

--- Android/app/src/main/java/com/example/jaebong/secerettalk/AddFriend.java
+++ Android/app/src/main/java/com/example/jaebong/secerettalk/AddFriend.java
@@ -1,39 +1,24 @@
 package com.example.jaebong.secerettalk;
 
-import android.support.v7.app.ActionBarActivity;
+import android.content.Context;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuItem;
+import android.support.v7.app.ActionBarActivity;
+import android.view.inputmethod.InputMethodManager;
+import android.widget.EditText;
 
 
 public class AddFriend extends ActionBarActivity {
+
+    private EditText tagId;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_add_friend);
+
+        tagId = (EditText)findViewById(R.id.addFriend_editText_TagId);
+
+
     }
 
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.menu_add_friend, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-
-        //noinspection SimplifiableIfStatement
-        if (id == R.id.action_settings) {
-            return true;
-        }
-
-        return super.onOptionsItemSelected(item);
-    }
 }
--- Android/app/src/main/java/com/example/jaebong/secerettalk/Join.java
+++ Android/app/src/main/java/com/example/jaebong/secerettalk/Join.java
@@ -1,11 +1,17 @@
 package com.example.jaebong.secerettalk;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.support.v7.app.ActionBarActivity;
 import android.view.View;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.TextView;
+
+import java.lang.reflect.Field;
+import java.util.Calendar;
+import java.util.GregorianCalendar;
 
 
 public class Join extends ActionBarActivity implements View.OnClickListener {
@@ -14,11 +20,23 @@ public class Join extends ActionBarActivity implements View.OnClickListener {
     private LinearLayout birthLayout;
     private EditText nickName;
     private TextView birth;
+    private int year;
+    private String date;
+    private int month;
+    private int day;
+    DatePickerDialog dialog;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_join);
+
+        GregorianCalendar calendar = new GregorianCalendar();
+        year = calendar.get(Calendar.YEAR);
+        month = calendar.get(Calendar.MONTH);
+        day = calendar.get(Calendar.DAY_OF_YEAR);
+
+        dialog = new DatePickerDialog(this,dateSetListener,year,month,day);
 
         birthLayout = (LinearLayout) findViewById(R.id.join_layout_birth);
 
@@ -26,10 +44,41 @@ public class Join extends ActionBarActivity implements View.OnClickListener {
         birth = (TextView)findViewById(R.id.join_tv_birth);
 
         birthLayout.setOnClickListener(this);
+
+        findAndHideField(dialog, "mDaySpinner");
+        findAndHideField(dialog, "mMonthSpinner");
     }
+
+    private void findAndHideField(DatePickerDialog datepicker, String name) {
+        try {
+            Field field = DatePicker.class.getDeclaredField(name);
+            field.setAccessible(true);
+            View fieldInstance = (View) field.get(datepicker);
+            fieldInstance.setVisibility(View.GONE);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+
+    private DatePickerDialog.OnDateSetListener dateSetListener = new DatePickerDialog.OnDateSetListener() {
+        @Override
+        public void onDateSet(DatePicker view, int year, int monthOfYear, int dayOfMonth) {
+            //picker에서 입력받은 생일을 저장
+            date = String.format("%d", year);
+            //birthday에 date정보를 string으로 저장
+            birth.setText(date);
+        }
+    };
+
 
     @Override
     public void onClick(View v) {
+        switch(v.getId()){
+            case R.id.join_layout_birth:
+                dialog.show();
+                break;
 
+        }
     }
+
 }
