08024b40703f8b13e7b058a3a7bb1a682924e08b
CK Kim <msty.kim@gmail.com>
2015-05-05 14:23:22 +0900

    Add models and ORM lib (realm.io)

---
+++ iOS/secretchat/SecretChat/Models/Friends.h
@@ -0,0 +1,24 @@
+//
+//  Friends.h
+//  SecretChat
+//
+//  Created by 김창규 on 2015. 5. 4..
+//  Copyright (c) 2015년 the.accidental.billionaire. All rights reserved.
+//
+
+#import <Realm/Realm.h>
+
+@interface Friends : RLMObject
+@property NSString* address;
+@property NSString* nickname;
+@property NSString* profileImg;
+@property NSString* bloodType;
+@property int   age;
+@property int   sex;
+@property NSString* level;
+
+@end
+
+// This protocol enables typed collections. i.e.:
+// RLMArray<Friends>
+RLM_ARRAY_TYPE(Friends)
