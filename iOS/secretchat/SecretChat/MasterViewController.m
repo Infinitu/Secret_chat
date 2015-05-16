be41201610999aa957af3437062b2107740ac6fd
CK Kim <msty.kim@gmail.com>
2015-05-16 18:19:09 +0900

    change MasterView to CollectionViewController.

--- iOS/secretchat/SecretChat/MasterViewController.m
+++ iOS/secretchat/SecretChat/MasterViewController.m
@@ -11,6 +11,9 @@
 #import "NetworkManager.h"
 #import "Version.h"
 #import "AppDelegate.h"
+#import "FriendsCollectionViewCell.h"
+#import <SDWebImage/UIImageView+WebCache.h>
+
 
 @interface MasterViewController ()
 
@@ -33,14 +36,10 @@
     NSLog(@"%@",[RLMRealm defaultRealmPath]);
     for(Friend *fr in result)
         [self.objects addObject:fr];
+
+    [self.collectionView setContentInset:UIEdgeInsetsMake(20, 20, 20, 20)];
 }
 
-
--(void)opened:(NSNotification*)noti{
-    UIDevice* device = [UIDevice currentDevice];
-    NSString* uuid = [[device identifierForVendor] UUIDString];
-//    sendMessage(0x1001,(uint8_t*)([[NSString stringWithFormat:@"%@|%@|%@|%@|%@|%@|",PROTOCOL_VERSION,uuid,ACCESSTOKEN_HARDCODING,device.systemVersion,APP_VERSION,device.localizedModel] cStringUsingEncoding:NSUTF8StringEncoding]));
-}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
@@ -51,51 +50,115 @@
 
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
     if ([[segue identifier] isEqualToString:@"showDetail"]) {
-        NSIndexPath *indexPath = [self.tableView indexPathForSelectedRow];
-        NSDate *object = self.objects[indexPath.row];
+        NSIndexPath *indexPath = [self.collectionView indexPathsForSelectedItems][0];
+        NSDate *object = self.objects[(NSUInteger) (indexPath.row-1)];
         [[segue destinationViewController] setDetailItem:object];
     }
 }
 
 #pragma mark - Table View
 
-- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+//- (NSInteger)numberOfSectionsInTableView:(UITableView *)tableView {
+//    return 1;
+//}
+//
+//- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
+//    return self.objects.count;
+//}
+//
+//- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
+//    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell" forIndexPath:indexPath];
+//
+//    NSDate *object = self.objects[indexPath.row];
+//    cell.textLabel.text = ((Friend*)object).nickname;
+//    return cell;
+//}
+//
+//- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
+//    // Return NO if you do not want the specified item to be editable.
+//    return YES;
+//}
+//
+//- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
+//    if (editingStyle == UITableViewCellEditingStyleDelete) {
+//        [self.objects removeObjectAtIndex:indexPath.row];
+//        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
+//    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
+//        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
+//    }
+//}
+//
+
+
+
+/*
+ #pragma mark - Navigation
+ 
+ // In a storyboard-based application, you will often want to do a little preparation before navigation
+ - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+ // Get the new view controller using [segue destinationViewController].
+ // Pass the selected object to the new view controller.
+ }
+ */
+
+#pragma mark <UICollectionViewDataSource>
+
+- (NSInteger)numberOfSectionsInCollectionView:(UICollectionView *)collectionView {
     return 1;
 }
 
-- (NSInteger)tableView:(UITableView *)tableView numberOfRowsInSection:(NSInteger)section {
-    return self.objects.count;
+
+- (NSInteger)collectionView:(UICollectionView *)collectionView numberOfItemsInSection:(NSInteger)section {
+    return self.objects.count+1;
 }
 
-- (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
-    UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"Cell" forIndexPath:indexPath];
+- (UICollectionViewCell *)collectionView:(UICollectionView *)collectionView cellForItemAtIndexPath:(NSIndexPath *)indexPath {
+    if(indexPath.row ==0)
+        return [collectionView dequeueReusableCellWithReuseIdentifier:@"newRandom"forIndexPath:indexPath];
+    FriendsCollectionViewCell *cell = [collectionView dequeueReusableCellWithReuseIdentifier:@"friend" forIndexPath:indexPath];
+    cell.friend = self.objects[(NSUInteger) (indexPath.row-1)];
+    cell.nicknameLabel.text = cell.friend.nickname;
+    [cell.profileImageView sd_setImageWithURL:[NSURL URLWithString:cell.friend.profileImg]];
 
-    NSDate *object = self.objects[indexPath.row];
-    cell.textLabel.text = ((Friend*)object).nickname;
     return cell;
 }
 
-- (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
-    // Return NO if you do not want the specified item to be editable.
-    return YES;
+- (CGSize)collectionView:(UICollectionView *)collectionView
+                  layout:(UICollectionViewLayout *)collectionViewLayout
+  sizeForItemAtIndexPath:(NSIndexPath *)indexPath{
+    return CGSizeMake(150, 158);
 }
 
-- (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
-    if (editingStyle == UITableViewCellEditingStyleDelete) {
-        [self.objects removeObjectAtIndex:indexPath.row];
-        [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
-    } else if (editingStyle == UITableViewCellEditingStyleInsert) {
-        // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view.
-    }
-}
+#pragma mark <UICollectionViewDelegate>
 
+/*
+ // Uncomment this method to specify if the specified item should be highlighted during tracking
+ - (BOOL)collectionView:(UICollectionView *)collectionView shouldHighlightItemAtIndexPath:(NSIndexPath *)indexPath {
+	return YES;
+ }
+ */
 
+/*
+ // Uncomment this method to specify if the specified item should be selected
+ - (BOOL)collectionView:(UICollectionView *)collectionView shouldSelectItemAtIndexPath:(NSIndexPath *)indexPath {
+ return YES;
+ }
+ */
 
--(void)newmsg:(NSNotification*)noti{
-//    NSLog(@"%@",noti.userInfo);
-    
-    
-}
+/*
+ // Uncomment these methods to specify if an action menu should be displayed for the specified item, and react to actions performed on the item
+ - (BOOL)collectionView:(UICollectionView *)collectionView shouldShowMenuForItemAtIndexPath:(NSIndexPath *)indexPath {
+	return NO;
+ }
+ 
+ - (BOOL)collectionView:(UICollectionView *)collectionView canPerformAction:(SEL)action forItemAtIndexPath:(NSIndexPath *)indexPath withSender:(id)sender {
+	return NO;
+ }
+ 
+ - (void)collectionView:(UICollectionView *)collectionView performAction:(SEL)action forItemAtIndexPath:(NSIndexPath *)indexPath withSender:(id)sender {
+	
+ }
+ */
 
 
 @end
